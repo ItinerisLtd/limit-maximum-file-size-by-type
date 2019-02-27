b9de90d17da42fc7821333802ace77838febbc45
Tang Rufus <tangrufus@gmail.com>
2019-02-27 09:08:14 +0000

    Extract from PhySoc

---
+++ limit-maximum-upload-file-size.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Plugin Name:     Limit Maximum Upload File Size
+ * Plugin URI:      https://github.com/itinerisltd/limit-maximum-upload-file-size
+ * Description:     Limit maximum upload file size
+ * Version:         0.1.0
+ * Author:          Itineris Limited
+ * Author URI:      https://www.itineris.co.uk/
+ * Text Domain:     limit-maximum-upload-file-size
+ */
+
+namespace Itineris\LimitMaximumUploadFileSize;
+
+// If this file is called directly, abort.
+if (! defined('WPINC')) {
+    die;
+}
+
+function startsWith(string $text, string $prefix): bool
+{
+    return '' !== $prefix && 0 === strpos($text, $prefix);
+}
+
+add_filter( 'upload_size_limit', function (): int {
+    return 5242880; // 5 * 1024 * 1024 = 5MB.
+}, 999);
+
+add_filter('wp_handle_upload_prefilter', function (array $file): array {
+    $imageSizeLimit = 2097152; // 2 * 1024 * 1024 = 2MB
+
+    // TODO: Allow configuration.
+    if (! startsWith($file['type'], 'image/')) {
+        return $file;
+    }
+
+    if ($imageSizeLimit > $file['size']) {
+        return $file;
+    }
+
+    // Translators: %1$d is the maximum file upload size , %2$s is its unit.
+    $errorMessagePattern = __('Image files must be smaller than %1$d %2$s', 'limit-maximum-upload-file-size');
+    $file['error'] = sprintf(
+        $errorMessagePattern,
+        $imageSizeLimit / 1048576,
+        'MB'
+    );
+
+    return $file;
+}, 9999);
